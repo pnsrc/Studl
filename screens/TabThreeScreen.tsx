f69cc1c51f076271ab7c95d11542cc71a82b4a25
Alexey <pnsrc@narod.ru>
2021-07-26 13:06:32 +0700

    Update 0.0.9b
    We adding feedback button

--- screens/TabThreeScreen.tsx
+++ screens/TabThreeScreen.tsx
@@ -15,7 +15,7 @@ export default function TabThreeScreen() {
         source={require('../assets/images/adaptive-icon.png')}
       />
       <Text style={styles.title}>Studl</Text>
-      <Text style={styles.getStartedText}>Версия <Badge value="0.0.8b" status="warning" /> </Text>
+      <Text style={styles.getStartedText}>Версия <Badge value="0.0.9b" status="warning" /> </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Pressable onPress={aboutdesc}>
       <Text style={styles.helpLinkText}>Написанно на React Native</Text>      
@@ -25,6 +25,10 @@ export default function TabThreeScreen() {
       <Text style={styles.getStartedText}>Made by pnsrc with ❤️</Text>
       </Pressable>  
       <Text style={styles.getStartedText2}>Логотип и SplashScreen by @elliot_alderson01.</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Pressable onPress={() => Linking.openURL('mailto:pnsrc@yandex.ru?subject=Обратная%20связь%20Studl%200.0.9b')}>
+      <Text style={styles.getStartedText2}>Обратная связь</Text>
+      </Pressable>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Text style={styles.getStartedText2}>Ваши данные не воруются, Альбине не попадут.</Text>
       <Pressable onPress={onPressFunction}>
@@ -37,7 +41,7 @@ export default function TabThreeScreen() {
 }
 console.log('Запуск модуля О приложении')
 function aboutdesc() {
-  Alert.alert('Список использованного','React\nReact-Native\nAppCenter\nБиблиотеки: react-native-elements, React-native-webview. \n Отдельное спасибо AppCenter за деплоинг, тестирование, а также сбор логов с приложения.')
+  Alert.alert('Список использованных библиотек','React\nReact-Native\nAppCenter\nБиблиотеки: react-native-elements, React-native-webview. \n Отдельное спасибо AppCenter за деплоинг, тестирование, а также сбор логов с приложения.')
 }
 function github() {
   countgit++
